a0e0af65cba7db85703f3fa9c36c746099f0da54
Mehrdad Zade <mehrdad.azh@gmail.com>
2020-04-02 09:51:01 -0400

    find pair sum a+b=c+d in an array for hashing

--- Hashing.py
+++ Hashing.py
@@ -9,9 +9,8 @@ https://www.geeksforgeeks.org/first-element-occurring-k-times-array/
 
 https://www.geeksforgeeks.org/longest-subarray-sum-divisible-k/
 
-https://www.geeksforgeeks.org/find-four-elements-a-b-c-and-d-in-an-array-such-that-ab-cd/
 '''
-
+#how many times has something been repeated in a list
 def frequencyCount(array):
     #this can be done in java with hash
     map = dict()
@@ -20,8 +19,28 @@ def frequencyCount(array):
             map[e] += 1
         else:
             map[e] = 1
-    
+
     print(map)
-    
+
+#Test Case
 array = [1,3,4,2,7,5,3,2,5,1,1,7]
 frequencyCount(array)
+
+################################################################################
+#find a, b, c, d in an array where a+b = c+d
+def findPairs(arr):
+  n = len(arr)
+  sumOfPairs = dict()
+  for i in range(0, n-1):
+    for j in range(i+1, n):
+      pairSum = arr[i] + arr[j]
+      if pairSum in sumOfPairs.keys():
+        return arr[i], arr[j], sumOfPairs.get(pairSum)[0], sumOfPairs.get(pairSum)[1]
+      else:
+        sumOfPairs[pairSum] = (arr[i], arr[j])
+  return None, None, None, None
+
+#Test Case
+print(findPairs([3, 4, 7, 1, 2, 9, 8]))
+
+################################################################################
--- Stack.py
+++ Stack.py
@@ -1,11 +1,12 @@
-https://www.geeksforgeeks.org/stack-set-2-infix-to-postfix/
+#https://www.geeksforgeeks.org/stack-set-2-infix-to-postfix/
 
-https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
+#https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
 
-https://www.geeksforgeeks.org/next-greater-element/
+#https://www.geeksforgeeks.org/next-greater-element/
 
-https://www.geeksforgeeks.org/sorting-array-using-stacks/
+#https://www.geeksforgeeks.org/sorting-array-using-stacks/
 
+################################################################################
 def HanoiTower(n, Tower1, Tower2, Tower3):
   if n==1:
     print ("Move disk",n,"from tower",Tower1,"to tower",Tower2)
@@ -15,7 +16,6 @@ def HanoiTower(n, Tower1, Tower2, Tower3):
   HanoiTower(n-1, Tower3, Tower2, Tower1)
 
 
-
-#------------------HanoiTower---------------------------------
+#Test Case
 n=3
 HanoiTower(n, 'A', 'B', 'C')
