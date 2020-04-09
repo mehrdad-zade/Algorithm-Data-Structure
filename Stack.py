fc532820668ba93640a189644613c4eee65a9898
Mehrdad Zade <mehrdad.azh@gmail.com>
2020-04-08 22:49:53 -0400

    check for balanced paranthesis using stacks

--- Stack.py
+++ Stack.py
@@ -1,9 +1,5 @@
 #https://www.geeksforgeeks.org/stack-set-2-infix-to-postfix/
 
-#https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
-
-
-
 ################################################################################
 def HanoiTower(n, Tower1, Tower2, Tower3):
   if n==1:
@@ -57,9 +53,48 @@ def nextGreaterElement2(arr):
         arrElement = -1
         print(str(stackElement) + " --> " + str(arrElement))
 
+#Test Case
 arr = [4, 5, 2, 25, 20]
 #arr = [11, 13, 21, 3]
 print(nextGreaterElement1(arr))
 print(nextGreaterElement2(arr))
+
+################################################################################
+'''
+Given an expression string exp , write a program to examine whether the pairs
+and the orders of “{“,”}”,”(“,”)”,”[“,”]” are correct in exp.
+Input: exp = “[()]{}{[()()]()}”
+Output: Balanced
+
+Input: exp = “[(])”
+Output: Not Balanced
+'''
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 22:42:03 2020
+
+@author: mehrdadalemzadeh
+"""
+
+def isBalanced(exp):
+    stack = []
+    for e in exp:
+        if e in ["[", "{", "("]:
+            stack.append(e)
+        elif e in ["]", "}", ")"]:
+            temp = stack.pop()
+            if (temp == "(" and e != ")") or (temp == "{" and e != "}") or (temp == "[" and e != "]"):
+                return False
+    if len(stack) == 0:
+        return True
+    else:
+        return
+
+#Test Case
+exp1 = "[()]{}{[()()]()}"
+exp2 = "[(])"
+print("Expression 1 is balanced? ", isBalanced(exp1))
+print("Expression 2 is balanced? ", isBalanced(exp2))
 
 ################################################################################
