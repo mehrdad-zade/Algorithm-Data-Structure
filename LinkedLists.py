f1c3e8beef62640b7d56a7ad35c94ac47dbeb3ae
Mehrdad Zade <mehrdad.azh@gmail.com>
2020-04-20 09:45:04 -0400

    linked list length calculation

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,1 +1,41 @@
-https://www.geeksforgeeks.org/find-length-of-a-linked-list-iterative-and-recursive/
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def push(self, val):
+        if self.head == None:
+            self.head = Node(val)
+        else:
+            current = self.head
+            while current.next != None:
+                current = current.next
+            current.next = Node(val)
+    def length(self):
+        if self.head == None:
+            return 0
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.next
+        return count
+
+
+#Test Cases
+l = LinkedList()
+#insert nodes by tapping into the class variables
+l.head = Node(1)
+l.head.next = Node(2)
+l.head.next.next = Node(3)
+l.head.next.next.next = Node(4)
+#a neat way of putting values at the end of a linkedlist
+l.push(5)
+l.push(6)
+l.push(7)
+print("Linked List Length = ", l.length())
+
+###############################################################################
--- Recursion.py
+++
@@ -1,1 +0,0 @@
-https://www.geeksforgeeks.org/algorithms-gq/recursion-gq/
