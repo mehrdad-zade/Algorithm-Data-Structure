dfe2f5ae7c207c556830df206003f3279cc70282
Mehrdad Zade <mehrdad.azh@gmail.com>
2020-09-29 10:51:37 -0400

    dynamic to array

--- Arrays.py
+++ Arrays.py
@@ -215,3 +215,56 @@ str2 = "m"
 print(editDistDP(str1, str2, len(str1), len(str2))) 
 
 ##############################################################################
+'''
+Given a value N, if we want to make change for N cents, and we have infinite 
+supply of each of S = { S1, S2, .. , Sm} valued coins, how many ways can we 
+make the change? The order of coins doesn’t matter.
+
+For example, for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},
+{1,1,2},{2,2},{1,3}. So output should be 4. For N = 10 and S = {2, 5, 3, 6}, 
+there are five solutions: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. 
+So the output should be 5.
+
+yu can see the recursive solution for better understanding the way to solve this, but
+we can do it with dynamic programming to save time O(mn)
+
+'''
+
+# Returns the count of ways we can sum 
+# S[0...m-1] coins to get sum n 
+def count1(S, m, n ): 
+  
+    # If n is 0 then there is 1 
+    # solution (do not include any coin) 
+    if (n == 0): 
+        return 1
+  
+    # If n is less than 0 then no 
+    # solution exists 
+    if (n < 0): 
+        return 0; 
+  
+    # If there are no coins and n 
+    # is greater than 0, then no 
+    # solution exist 
+    if (m <=0 and n >= 1): 
+        return 0
+  
+    # count is sum of solutions (i)  
+    # including S[m-1] (ii) excluding S[m-1] 
+    return count( S, m - 1, n ) + count( S, m, n-S[m-1] );
+
+def count2(S, m, n):
+    table = [0 for i in range(n+1)]
+    table[0] = 1
+    for i in range(m):
+        for j in range(S[i], n+1):
+            table[j] += table[j-S[i]]
+    return table
+    
+    
+arr = [1, 2, 3] 
+m = len(arr) 
+n = 4
+print(count2(arr, m, n))
+#########################################################################################
