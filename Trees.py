f8446564987809b33382d2f3a8a6553b367a256d
Mehrdad Zade <mehrdad.azh@gmail.com>
2021-10-13 14:31:38 -0400

    robbery tree

--- Trees.py
+++ Trees.py
@@ -874,4 +874,39 @@ if __name__ == '__main__':
     root.left.right = newNode(8)  
     root.right.right = newNode(7)  
     averageOfLevels(root) 
+
+
+
+ #16############################################################################################################################################                
+'''
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
+
+Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,2,3,null,3,null,1]
+Output: 7
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+'''
+
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            if not root:
+                return [0,0]
+            l = dfs(root.left)
+            r = dfs(root.right)
+            return [root.val + l[1] + r[1], max(l) + max(r)]
+        return max(dfs(root))
+
+
+ #17############################################################################################################################################                
+
+
   
